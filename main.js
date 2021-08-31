986d70aad53a606ab47b574e7fce61c5d49765d8
huanmingchang <huanming1988@gmail.com>
2021-08-31 20:11:28 +0800

    feat: add dark mode function

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const input = document.getElementById("action__input_1");
+const darkModeToggle = document.getElementById("dark__mode__toggle");
 
 const toggleMenu = (event) => {
   const menu = document.getElementById("action__menu_1");
   menu.classList.toggle("hidden");
 };
 
+const darkModeToggleHandler = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+
 input.addEventListener("click", toggleMenu);
+darkModeToggle.addEventListener("change", darkModeToggleHandler);
